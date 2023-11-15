3d48c26d63aa8ddfe48d3399983de4bee69acfa7
roboticslover <135958089+roboticslover@users.noreply.github.com>
2023-11-15 15:56:09 +0530

    Update 1.py

--- 65/1.py
+++ 65/1.py
@@ -1,4 +1,6 @@
 # Static Method
+# Static Method: In Python, a static method is a method that belongs to a class rather than an instance of the class.
+# They can be called on the class itself, without creating an instance.
 
 class Math:
     def __init__(self, num):
